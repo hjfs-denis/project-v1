d3f7c04e13e7b98bb2ff5c48b02743427f9c8151
Denis <denisdevaraj@gmail.com>
2020-04-14 15:48:46 +0530

    User.java

--- eGalaxySB/src/main/java/com/egalaxy/user/entity/User.java
+++ eGalaxySB/src/main/java/com/egalaxy/user/entity/User.java
@@ -1,5 +1,78 @@
 package com.egalaxy.user.entity;
 
-public class User {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "USERS")
+public class User {
+	
+	@Id
+	@Column(name = "USER_ID")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int userId;
+	@Column(name = "USER_NAME")
+	private String userName;
+	@Column(name = "USER_PASSWORD")
+	private String userPassword;
+	@Column(name = "USER_DOB")
+	private String userDob;
+	@Column(name = "USER_GENDER")
+	private String userGender;
+	
+	User() {
+		
+	}
+	
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	
+	public int getUserId() {
+		return this.userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getUserPassword() {
+		return userPassword;
+	}
+
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+	public String getUserDob() {
+		return userDob;
+	}
+
+	public void setUserDob(String userDob) {
+		this.userDob = userDob;
+	}
+
+	public String getUserGender() {
+		return userGender;
+	}
+
+	public void setUserGender(String userGender) {
+		this.userGender = userGender;
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", userName=" + userName + ", userPassword=" + userPassword + ", userDob="
+				+ userDob + ", userGender=" + userGender + "]";
+	}
+
+	
 }
